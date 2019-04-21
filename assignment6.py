b0be7c0454b30647b67cd53a8cb7b23e64353620
AliTayeh <tayehas@vcu.edu>
2019-04-20 23:03:28 -0400

    add rectangle class

--- assignment6.py
+++ assignment6.py
@@ -1,10 +1,16 @@
 from Tkinter import *
+
+
 class CustomCanvas:
     __height=0
     __width=0
     def __init__(self, height, width):
         self.__height=height
         self.__width=width
+        master = Tk()
+        w = Canvas(master, width= int(height), height= int(width), bg="black")
+        w.pack()
+        master.mainloop()
     def getHeight(self):
         return self.__height
     def getWidth(self):
@@ -13,17 +19,16 @@ class CustomCanvas:
         return ("the canvas has a height of: {} and a width of: {}".format(self.__height,self.__width) )
 
 
-obj = CustomCanvas(500,500)
-master = Tk()
+class Rectangle:
+    __height=0
+    __width=0
+    __x=0
+    __y=0
+    
+    def __init__(height,width,x,y):
+        self.__height=height
+        self.__width=width
+        self.__x=x
+        self.__y=y
 
-w = Canvas(master, width= int(obj.getWidth()), height= int(obj.getHeight()), bg="blue")
-#c = w.create_rectangle(50, 25, 150, 75, fill="red")
 
-w.pack()
-
-
-
-
-master.mainloop()
-
-print(obj.getWidth())
