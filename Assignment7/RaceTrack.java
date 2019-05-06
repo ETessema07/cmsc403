d38ba3377c0531f552e070567288ce55f4bab059
AliTayeh <tayehas@vcu.edu>
2019-05-06 05:01:07 -0400

    add start,pause and reset functionality in RaceTrack

--- Assignment7/RaceTrack.java
+++ Assignment7/RaceTrack.java
@@ -7,13 +7,15 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class RaceTrack extends JPanel {
+public class RaceTrack extends JPanel implements Runnable{
     static BufferedImage car;
     static JPanel p;
     static JButton start;
     static JButton pause;
     static JButton reset;
     static JFrame f;
+    static int d = 20;
+    static Thread t;
     public RaceTrack(){
         p = new JPanel();
         p.setLayout(new FlowLayout());
@@ -24,12 +26,14 @@ public class RaceTrack extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Im starting");
+                d+=30;
             }
         });
         pause.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Im pausing");
+                d=d;
             }
         });
 
@@ -37,6 +41,7 @@ public class RaceTrack extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Im resetting");
+                d=20;
             }
         });
 
@@ -50,6 +55,7 @@ public class RaceTrack extends JPanel {
 
 
     public static void main(String [] strings){
+
         RaceTrack r = new RaceTrack();
         f = new JFrame();
         f.setContentPane(r);
@@ -59,9 +65,9 @@ public class RaceTrack extends JPanel {
         f.setSize(500, 200);
         f.setTitle("Race Track");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        f.setLocationRelativeTo(null);
+        f.repaint();
         f.setVisible(true);
-
 
     }
     @Override
@@ -71,21 +77,26 @@ public class RaceTrack extends JPanel {
             car = ImageIO.read(new File("sportive-car.png"));
 
         }catch (IOException e){
-            System.out.println("wrong path!");
+            System.out.println("wrong path for the car image, check the path in paintComponent() !");
         }
 
         g.setColor(Color.GRAY);
-        g.drawImage(car,20,39, this);
-        g.drawImage(car,20,89, this);
-        g.drawImage(car,20,139, this);
+
         g.drawRect(53,50,400,10);
         g.drawRect(53,100,400,10);
         g.drawRect(53,150,400,10);
         g.fillRect(53,50,400,10);
         g.fillRect(53,100,400,10);
         g.fillRect(53,150,400,10);
-
+        g.drawImage(car, d, 39, this);
+        g.drawImage(car,20,89, this);
+        g.drawImage(car,20,139, this);
+        repaint();
     }
 
 
+    @Override
+    public void run() {
+
+    }
 }
