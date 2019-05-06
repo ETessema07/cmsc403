8fefbeb91d84a9eb6eab66f45592f717a72cbb9b
AliTayeh <tayehas@vcu.edu>
2019-05-06 19:29:19 -0400

    finalize RaceTrack

--- Assignment7/RaceTrack.java
+++ Assignment7/RaceTrack.java
@@ -23,6 +23,8 @@ public class RaceTrack extends JPanel{
     static Thread racer3;
     static boolean go=true;
     static Random rand;
+    static String winner;
+    static boolean endRace = false;
 
     public RaceTrack(){
         p = new JPanel();
@@ -36,23 +38,30 @@ public class RaceTrack extends JPanel{
         p.add(reset);
     }
 
-    public static void buttons(){
+    public void buttons(){
         rand = new Random();
+
         start.addActionListener(new ActionListener() {
+            
             @Override
             public void actionPerformed(ActionEvent e) {
                 go =true;
+                endRace=false;
                 racer1 = new Thread(new Runnable() {
                     @Override
                     public void run() {
                         rand = new Random();
                         while(go){
                             try {
-                                int random = rand.nextInt(11)+1;
+                                int random = rand.nextInt(11);
                                 car1+=random;
-                                Thread.sleep(1000);
+                                haveWinner();
+                                if(!endRace) {
+                                    repaint();
+                                }
+                                Thread.sleep(50);
                             }catch (InterruptedException l){
-                                return;
+
                             }
                         }
                     }
@@ -65,11 +74,11 @@ public class RaceTrack extends JPanel{
                         rand = new Random();
                         while(go){
                             try {
-                                int random = rand.nextInt(11)+1;
+                                int random = rand.nextInt(11);
                                 car2+=random;
-                                Thread.sleep(1000);
+                                Thread.sleep(50);
                             }catch (InterruptedException l){
-                                return;
+
                             }
                         }
                     }
@@ -82,23 +91,25 @@ public class RaceTrack extends JPanel{
                         rand = new Random();
                         while(go){
                             try {
-                                int random = rand.nextInt(11)+1;
+                                int random = rand.nextInt(11);
                                 car3+=random;
-                                Thread.sleep(1000);
+                                Thread.sleep(50);
                             }catch (InterruptedException l){
-                                return;
+
                             }
                         }
                     }
                 });
                 racer3.start();
 
+
+
             }
         });
         pause.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Im pausing");
+
                 go = false;
             }
         });
@@ -106,11 +117,12 @@ public class RaceTrack extends JPanel{
         reset.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Im resetting");
                 car1=20;
                 car2=20;
                 car3=20;
                 go = false;
+                repaint();
+
             }
         });
     }
@@ -118,10 +130,9 @@ public class RaceTrack extends JPanel{
 
 
     public static void main(String [] strings){
-
-        RaceTrack r = new RaceTrack();
         f = new JFrame();
-        f.setContentPane(r);
+        f.setResizable(false);
+        f.setContentPane(new RaceTrack());
 
         f.add(p);
 
@@ -129,11 +140,38 @@ public class RaceTrack extends JPanel{
         f.setTitle("Race Track");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLocationRelativeTo(null);
-        f.repaint();
         f.setVisible(true);
 
 
+
     }
+
+     public synchronized void haveWinner(){
+        if (car1 >= 420 || car2 >= 420 || car3 >= 420) {
+            if (car1 >= 420 && car1> car2 && car1>car3) {
+                winner = "car1";
+                System.out.println(winner+"has won");
+            }
+            else if (car2 >= 420 && car2> car1 && car2>car3) {
+                winner = "car2";
+                System.out.println(winner+"has won");
+            }
+            else if(car3 >= 420 && car3> car2 && car3>car1) {
+                winner = "car3";
+                System.out.println(winner+"has won");
+            }
+            JOptionPane.showMessageDialog(null,"the winner is:  "+ winner);
+            go=false;
+            endRace=true;
+//            car1 = 20;
+//            car2 = 20;
+//            car3 = 20;
+//            repaint();
+
+
+        }
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -141,7 +179,7 @@ public class RaceTrack extends JPanel{
             car = ImageIO.read(new File("sportive-car.png"));
 
         }catch (IOException e){
-            System.out.println("wrong path for the car image, check the path in paintComponent() !");
+            System.out.println("wrong path for the car image, check the path in paintComponent()!");
         }
 
         g.setColor(Color.GRAY);
@@ -155,25 +193,11 @@ public class RaceTrack extends JPanel{
         g.drawImage(car, car1, 39, this);
         g.drawImage(car,car2,89, this);
         g.drawImage(car,car3,139, this);
-        repaint();
+
+
+
     }
 
 
-//    @Override
-//    public synchronized void run() {
-//        start.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//                    System.out.println("Im starting");
-//                    d += 10;
-//                    try {
-//                        Thread.sleep(3000);
-//                    } catch (InterruptedException a) {
-//
-//                    }
-//
-//            }
-//        });
-//    }
+
 }
